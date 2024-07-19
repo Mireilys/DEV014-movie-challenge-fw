ce9a2f61cc838b5e5cfc437c0d6cdd3a0659b1be
Mireilys <160797302+Mireilys@users.noreply.github.com>
2024-07-19 12:15:03 -0400

    Remove my-react-app folder

--- my-react-app/babel.config.ts
+++
@@ -1,6 +0,0 @@
-module.exports = { /* eslint-disable */
-    presets: [
-      '@babel/preset-env', // Preset para convertir JavaScript moderno a JavaScript compatible con versiones anteriores
-      '@babel/preset-typescript', // Preset para habilitar TypeScript
-    ],
-  };
--- my-react-app/src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from '../App'; // Asegúrate de que la ruta sea correcta
-
-test('renders Vite + React heading', () => {
-    render(<App />);
-    const headingElement = screen.getByText(/Vite \+ React/i);
-    expect(headingElement).toBeInTheDocument();
-});
